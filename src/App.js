996560015750467725701955d63891e8cc3319cb
Waqar <mwaqar29@gmail.com>
2018-09-13 13:00:39 +0530

    First deploy

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ class App extends Component {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input) 
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://evening-castle-91994.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
